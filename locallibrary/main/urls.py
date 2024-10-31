fe3ecccc172342ec47e6c102f8b8e7ec92b135ed
Владислав Стретович <gr424_stvev@lab-402-1.tomtit-tomsk.ru>
2024-10-31 11:02:36 +0700

    commit 2.1

--- locallibrary/main/urls.py
+++ locallibrary/main/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
-
-
 ]
