280871fd86dd252b78ac565e0321f2543b230e4c
Владислав Стретович <gr424_stvev@lab-402-1.tomtit-tomsk.ru>
2024-11-01 11:37:03 +0700

    lastcommit 2.1

--- locallibrary/main/views.py
+++ locallibrary/main/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
 def home(request):
     return render(request, 'main/home.html')
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'main/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
