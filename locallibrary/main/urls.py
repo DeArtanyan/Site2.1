e487175055e04471830cf9c860afc8ffc6a09997
Владислав Стретович <gr424_stvev@lab-402-1.tomtit-tomsk.ru>
2024-11-02 11:18:07 +0700

    vetka1.1

--- locallibrary/main/urls.py
+++ locallibrary/main/urls.py
@@ -5,7 +5,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('login/', views.custom_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('create_request/', views.create_request, name='create_request'),
 ]
